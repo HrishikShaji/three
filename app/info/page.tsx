61db1be8f15f254706643efff5bff0ff4d2f825a
Hrishik Shaji <hrishikminishaji@gmail.com>
2025-06-29 21:54:52 +0530

    page transition added

---
+++ app/info/page.tsx
@@ -0,0 +1,14 @@
+
+export default function Page() {
+  return (
+    <div className="info">
+      <div className="col">
+        <img src="https://images.pexels.com/photos/41949/earth-earth-at-night-night-lights-41949.jpeg" alt="" />
+      </div>
+      <div className="col">
+        <p>paragraph</p>
+      </div>
+
+    </div>
+  )
+}
---
+++ app/projects/page.tsx
@@ -0,0 +1,12 @@
+export default function Page() {
+  return (
+    <div className="projects">
+      <div className="images">
+        <img src="https://images.pexels.com/photos/41949/earth-earth-at-night-night-lights-41949.jpeg" alt="" />
+        <img src="https://images.pexels.com/photos/41949/earth-earth-at-night-night-lights-41949.jpeg" alt="" />
+        <img src="https://images.pexels.com/photos/41949/earth-earth-at-night-night-lights-41949.jpeg" alt="" />
+        <img src="https://images.pexels.com/photos/41949/earth-earth-at-night-night-lights-41949.jpeg" alt="" />
+      </div>
+    </div>
+  )
+}
