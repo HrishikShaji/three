f747915e22c31148ec050f66f112fcec4b4c5094
Hrishik Shaji <hrishikminishaji@gmail.com>
2025-07-05 20:51:15 +0530

    sample gallery added

---
+++ app/components/Gallery.tsx
@@ -0,0 +1,75 @@
+"use client"
+import { useGSAP } from "@gsap/react"
+import gsap from "gsap"
+import { Flip } from "gsap/all"
+import { useRef, useState } from "react"
+
+// Register the plugin
+gsap.registerPlugin(Flip)
+
+export default function Gallery() {
+	const card1 = useRef<HTMLDivElement>(null)
+	const card2 = useRef<HTMLDivElement>(null)
+	const card3 = useRef<HTMLDivElement>(null)
+	const topContainer = useRef<HTMLDivElement>(null)
+	const bottomContainer = useRef<HTMLDivElement>(null)
+
+	useGSAP(() => {
+
+		const timeline = gsap.timeline()
+
+		timeline.fromTo(card1.current, {
+			scaleX: "150%",
+		}, {
+			scaleX: "100%",
+			transformOrigin: "left"
+		}, 0).fromTo(card2.current, {
+			scaleX: "0%",
+		}, {
+			scaleX: "100%",
+			transformOrigin: "right"
+		}, 0).fromTo(card3.current, {
+			scaleX: "0%",
+		}, {
+			scaleX: "100%",
+			transformOrigin: "right"
+		}, 0)
+
+
+		gsap.fromTo(topContainer.current, {
+			scaleY: "150%",
+		}, {
+			scaleY: "100%",
+			transformOrigin: "top"
+		})
+
+		gsap.fromTo(bottomContainer.current, {
+			scaleY: "0%"
+		}, {
+			scaleY: "100%",
+			transformOrigin: 'bottom'
+		})
+
+
+	}, {})
+
+	return (
+		<div className="w-full h-full p-10 bg-gray-100 min-h-screen">
+			<div className="w-full h-full flex flex-col gap-5">
+				<div ref={topContainer} className="flex-2/3 flex gap-5 w-full h-full">
+					<div ref={card1} className="h-full flex-2/3 w-full bg-green-500"></div>
+					<div className="flex flex-1/3 flex-col gap-5 h-full w-full">
+						<div ref={card2} className="h-[50%] w-full bg-red-500"></div>
+						<div ref={card3} className="h-[50%] w-full bg-red-500"></div>
+					</div>
+				</div>
+				<div ref={bottomContainer} className="flex-1/3 flex gap-5">
+					<div className="flex-1/4 h-full bg-purple-500"></div>
+					<div className="flex-1/4 h-full bg-purple-500"></div>
+					<div className="flex-1/4 h-full bg-purple-500"></div>
+					<div className="flex-1/4 h-full bg-purple-500"></div>
+				</div>
+			</div>
+		</div>
+	)
+}
--- app/work/page.tsx
+++ app/work/page.tsx
@@ -1,7 +1,10 @@
+import Gallery from "../components/Gallery";
+
 export default function Page() {
   return (
-    <div className="h-screen bg-blue-500 w-full flex justify-center items-center ">
-      <h1 className="text-9xl">WORK</h1>
+    <div className="h-screen ">
+      <Gallery />
+
     </div>
   )
 }
