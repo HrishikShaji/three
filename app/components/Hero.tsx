9280d05618f0418de5de121991afb362fa2f77ba
Hrishik Shaji <hrishikminishaji@gmail.com>
2025-07-05 13:52:31 +0530

    preloader slide done

--- app/components/Hero.tsx
+++ app/components/Hero.tsx
@@ -1,21 +1,44 @@
+"use client"
+
+import { useGSAP } from "@gsap/react";
+import gsap from "gsap";
 import Image from "next/image";
+import { useRef } from "react";
 
 export default function Hero() {
+	const sectionOne = useRef<HTMLDivElement>(null)
+	const sectionTwo = useRef<HTMLDivElement>(null)
+	useGSAP(() => {
+		gsap.to(sectionOne.current, {
+			xPercent: -100,
+			duration: 2
+		})
+		gsap.to(sectionTwo.current, {
+			xPercent: -100,
+			duration: 2
+		})
+
+
+	}, {})
+
 	return (
-		<div className="h-screen w-full p-5">
-			<h1 className="text-[230px] leading-[200px] m-0 tracking-tighter font-semibold px-0">HRISHIK SHAJI </h1>
-			<div className="flex h-[50vh] pl-4">
-				<div className="flex-1  h-full flex flex-col gap-2">
-					<Image src="https://images.pexels.com/photos/6153354/pexels-photo-6153354.jpeg"
-						height={1000} width={1000} alt="image" className="h-full w-full object-cover" />
-					<h1 className="text-xl font-medium">Bringing designs to life with pixel-perfect precision and smooth animations.</h1>
-				</div>
-				<div className="flex-1 h-full flex justify-center items-center">
-					<div className="flex flex-col gap-4 w-[50%]">
-						<p>I'm a 32 years-old French front-end developer who cares about design. My focus is on precise integration and crafting innovative interactions.</p>
-						<div className="flex gap-2">
-							<button className="rounded-3xl font-medium p-3 border-[1px] border-black">See my projects</button>
-							<button className="rounded-3xl font-medium p-3 border-[1px] border-black">Quick overview</button>
+		<div className="flex w-[100vw]  overflow-hidden">
+			<div ref={sectionOne} className="h-screen shrink-0 w-[100vw] bg-black"></div>
+			<div ref={sectionTwo} className="h-screen shrink-0 w-[100vw] p-5">
+				<h1 className="text-[230px] leading-[200px] m-0 tracking-tighter font-semibold px-0">HRISHIK SHAJI </h1>
+				<div className="flex h-[50vh] pl-4">
+					<div className="flex-1  h-full flex flex-col gap-2">
+						<Image src="https://images.pexels.com/photos/6153354/pexels-photo-6153354.jpeg"
+							height={1000} width={1000} alt="image" className="h-full w-full object-cover" />
+						<h1 className="text-xl font-medium">Bringing designs to life with pixel-perfect precision and smooth animations.</h1>
+					</div>
+					<div className="flex-1 h-full flex justify-center items-center">
+						<div className="flex flex-col gap-4 w-[50%]">
+							<p>I'm a 32 years-old French front-end developer who cares about design. My focus is on precise integration and crafting innovative interactions.</p>
+							<div className="flex gap-2">
+								<button className="rounded-3xl font-medium p-3 border-[1px] border-black">See my projects</button>
+								<button className="rounded-3xl font-medium p-3 border-[1px] border-black">Quick overview</button>
+							</div>
 						</div>
 					</div>
 				</div>
@@ -23,3 +46,7 @@ export default function Hero() {
 		</div>
 	)
 }
+
+{/*
+
+*/}
