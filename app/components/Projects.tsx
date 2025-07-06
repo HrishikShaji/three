e32b5ed0b0507eae937e97246114a2a9a176b7c0
Hrishik Shaji <hrishikminishaji@gmail.com>
2025-07-06 21:02:01 +0530

    projects done

--- app/components/Projects.tsx
+++ app/components/Projects.tsx
@@ -2,6 +2,7 @@
 
 import { projects } from "@/lib/constants"
 import gsap from "gsap"
+import { ArrowUpRight } from "lucide-react"
 import Image from "next/image"
 import React, { useEffect, useRef, useState } from "react"
 
@@ -16,18 +17,22 @@ export default function Projects() {
 	function onMouseMove(e: React.MouseEvent, index: number) {
 		console.log("mouse over", containerRef.current?.clientWidth, e.clientX)
 		const containerWidth = containerRef.current?.clientWidth
+		const offsetX = 100
+		const offsetY = 300
+		const previewWidth = 320
+
 		if (!containerWidth) return
-		const maxRight = containerWidth - 260
+		const maxRight = containerWidth - (previewWidth + offsetX)
 		if (e.clientX < maxRight) {
 			setCurrentImg(projects[index].img)
 			gsap.to(previewRef.current, {
-				x: e.clientX + 100,
-				y: e.clientY - 50
+				x: e.clientX + offsetX,
+				y: e.clientY - offsetY,
 			})
 		} else {
 			setCurrentImg(projects[index].img)
 			gsap.to(previewRef.current, {
-				y: e.clientY - 50
+				y: e.clientY - offsetY
 			})
 		}
 	}
@@ -35,6 +40,8 @@ export default function Projects() {
 	function onMouseEnter(e: React.MouseEvent, index: number) {
 		const element = refs.current[index]
 		const overlay = element?.getElementsByClassName("overlay")
+		const linkText = element?.getElementsByClassName("link-text")
+		const icon = element?.getElementsByClassName("icon")
 		if (overlay && overlay[0]) {
 			const overlayElement = overlay[0]
 			gsap.fromTo(overlayElement, {
@@ -45,12 +52,30 @@ export default function Projects() {
 				scaleY: 1,
 				duration: .5,
 				ease: "power2.out"
+			})
+		}
+
+		if (linkText && linkText[0]) {
+			gsap.to(linkText, {
+				x: 10,
+				color: "#fff",
+				duration: .5
+			})
+		}
+
+		if (icon && icon[0]) {
+			gsap.to(icon, {
+				x: -10,
+				color: "#fff",
+				duration: .5
 			})
 		}
 	}
 	function onMouseLeave(e: React.MouseEvent, index: number) {
 		const element = refs.current[index]
 		const overlay = element?.getElementsByClassName("overlay")
+		const linkText = element?.getElementsByClassName("link-text")
+		const icon = element?.getElementsByClassName("icon")
 		if (overlay && overlay[0]) {
 			const overlayElement = overlay[0]
 			gsap.fromTo(overlayElement, {
@@ -60,35 +85,54 @@ export default function Projects() {
 				background: "#fff",
 				scaleY: 0,
 				duration: .5,
-				ease: "power2.out"
+				ease: "power2.out",
+			})
+		}
+		if (linkText && linkText[0]) {
+			gsap.to(linkText, {
+				x: 0,
+				color: "#000",
+				duration: .5
+			})
+		}
+		if (icon && icon[0]) {
+			gsap.to(icon, {
+				x: 0,
+				color: "#000",
+				duration: .5
 			})
 		}
 	}
 
 	return (
-		<div className="w-full h-full relative" ref={containerRef}>
-			{currentImg ?
-				<div
-					ref={previewRef}
-					className="size-80 border-black border-8 pointer-events-none z-40  absolute"
-				>
-					<Image src={currentImg} fill className="object-cover" alt="" />
-				</div> : null}
-			{projects.map((project, i) => (
-				<div
-					ref={(el) => {
-						refs.current[i] = el
-					}}
-					onMouseLeave={(e) => onMouseLeave(e, i)}
-					onMouseEnter={(e) => onMouseEnter(e, i)}
-					onMouseMove={(e) => onMouseMove(e, i)}
-					key={i}
-					className="p-5 relative border-b-[1px] border-gray-300 bg-white">
-					<div id={`${i}`} className="overlay h-full w-full left-0 top-0 absolute   mix-blend-difference" />
-					<h1 className="text-2xl text-black">{project.title}</h1>
-				</div>
-			))
-			}
-		</div >
+		<div className="px-10 w-full h-full">
+			<h1 className="text-7xl font-normal mb-5">More Projects</h1>
+			<div className="w-full h-full relative" ref={containerRef}>
+				{currentImg ?
+					<div
+						ref={previewRef}
+						className="size-80 border-black border-8 pointer-events-none z-40  absolute"
+					>
+						<Image src={currentImg} fill className="object-cover" alt="" />
+					</div> : null}
+				{projects.map((project, i) => (
+					<div
+						ref={(el) => {
+							refs.current[i] = el
+						}}
+						onMouseLeave={(e) => onMouseLeave(e, i)}
+						onMouseEnter={(e) => onMouseEnter(e, i)}
+						onMouseMove={(e) => onMouseMove(e, i)}
+						key={i}
+						className="px-5 py-10 flex justify-between relative border-b-[1px] border-gray-300 bg-white">
+						<div id={`${i}`} className="overlay h-full w-full left-0 top-0 absolute   mix-blend-difference" />
+						<h1 className="link-text text-2xl text-black">{project.title}</h1>
+						<span className="icon"><ArrowUpRight /></span>
+					</div>
+				))
+				}
+			</div >
+
+		</div>
 	)
 }
--- app/work/page.tsx
+++ app/work/page.tsx
@@ -1,11 +1,13 @@
 import Gallery from "../components/Gallery";
 import Overview from "../components/Overview";
+import Projects from "../components/Projects";
 
 export default function Page() {
   return (
     <div className="">
       <Gallery />
       <Overview />
+      <Projects />
     </div>
   )
 }
