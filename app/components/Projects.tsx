27255c12522b3bb3736c169f90218a7aa1ed2ec9
Hrishik Shaji <hrishikminishaji@gmail.com>
2025-07-06 15:19:54 +0530

    projects anim done

--- app/components/Projects.tsx
+++ app/components/Projects.tsx
@@ -2,15 +2,40 @@
 
 import { projects } from "@/lib/constants"
 import gsap from "gsap"
-import { useRef } from "react"
+import Image from "next/image"
+import React, { useEffect, useRef, useState } from "react"
 
 export default function Projects() {
 	const refs = useRef<(HTMLDivElement | null)[]>([])
+	const previewRef = useRef<HTMLDivElement>(null)
+	const containerRef = useRef<HTMLDivElement>(null)
+	const [currentImg, setCurrentImg] = useState("")
+
+
+
+	function onMouseMove(e: React.MouseEvent, index: number) {
+		console.log("mouse over", containerRef.current?.clientWidth, e.clientX)
+		const containerWidth = containerRef.current?.clientWidth
+		if (!containerWidth) return
+		const maxRight = containerWidth - 260
+		if (e.clientX < maxRight) {
+			setCurrentImg(projects[index].img)
+			gsap.to(previewRef.current, {
+				x: e.clientX + 100,
+				y: e.clientY - 50
+			})
+		} else {
+			setCurrentImg(projects[index].img)
+			gsap.to(previewRef.current, {
+				y: e.clientY - 50
+			})
+		}
+	}
+
 	function onMouseEnter(e: React.MouseEvent, index: number) {
 		const element = refs.current[index]
 		const overlay = element?.getElementsByClassName("overlay")
 		if (overlay && overlay[0]) {
-			console.log("overlay", overlay[0])
 			const overlayElement = overlay[0]
 			gsap.fromTo(overlayElement, {
 				scaleY: 0,
@@ -27,7 +52,6 @@ export default function Projects() {
 		const element = refs.current[index]
 		const overlay = element?.getElementsByClassName("overlay")
 		if (overlay && overlay[0]) {
-			console.log("overlay", overlay[0])
 			const overlayElement = overlay[0]
 			gsap.fromTo(overlayElement, {
 				scaleY: 1,
@@ -42,20 +66,29 @@ export default function Projects() {
 	}
 
 	return (
-		<div className="w-full h-full">
+		<div className="w-full h-full relative" ref={containerRef}>
+			{currentImg ?
+				<div
+					ref={previewRef}
+					className="size-80 border-black border-8 pointer-events-none z-40  absolute"
+				>
+					<Image src={currentImg} fill className="object-cover" alt="" />
+				</div> : null}
 			{projects.map((project, i) => (
 				<div
 					ref={(el) => {
-						refs.current.push(el)
+						refs.current[i] = el
 					}}
 					onMouseLeave={(e) => onMouseLeave(e, i)}
 					onMouseEnter={(e) => onMouseEnter(e, i)}
+					onMouseMove={(e) => onMouseMove(e, i)}
 					key={i}
 					className="p-5 relative border-b-[1px] border-gray-300 bg-white">
 					<div id={`${i}`} className="overlay h-full w-full left-0 top-0 absolute   mix-blend-difference" />
 					<h1 className="text-2xl text-black">{project.title}</h1>
 				</div>
-			))}
-		</div>
+			))
+			}
+		</div >
 	)
 }
